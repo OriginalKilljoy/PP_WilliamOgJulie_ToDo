cafdaa7534e04d0b2778136339e35b1a2942bd51
OriginalKilljoy <162512500+OriginalKilljoy@users.noreply.github.com>
2024-12-12 14:10:59 +0100

    Project started

---
+++ PP_WilliamOgJulie_ToDo/Task.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PP_WilliamOgJulie_ToDo
+{
+    internal class Task
+    {
+        public string Name;
+        public string DoDate;
+        public bool Done = false;
+
+        public Task(string name, string doDate, bool done)
+        {
+            Name = name;
+            DoDate = doDate;
+            Done = done;
+        }
+    }
+
+
+}
